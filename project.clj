bfbb16d9c5a47411b80d21ba5405589ae6814fd1
Christopher Penrose <christopherpenrose@gmail.com>
2016-12-02 15:08:48 -0800

    upped project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject waffletower/serum "0.1.0"
+(defproject waffletower/serum "0.1.1"
   :description "Clojure library of utility functions and macros"
   :url "https://github.com/waffletower/serum"
   :license {:name "Eclipse Public License"
