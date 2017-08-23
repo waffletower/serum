b00b2785ec9c45cfc8e11992709c928ac790e8ba
Christopher Penrose <bonus@leisuresonic.com>
2017-08-22 23:03:38 -0700

    added deployment config

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  :deploy-repositories [["clojars-https" {:url "https://clojars.org/repo"
+                                          :username :env/clojars_user
+                                          :password :env/clojars_password}]]
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]]}})
