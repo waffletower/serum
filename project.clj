a69c36ce7cb4c4dc45ac3a0f8b99c11f42f6e2bd
Christopher Penrose <waffletower@users.noreply.github.com>
2018-05-09 22:47:51 -0700

    gave in and added logging

--- project.clj
+++ project.clj
@@ -6,10 +6,11 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.4.474"]
                  [camel-snake-kebab "0.4.0"]
-                 [slingshot "0.12.2"]]
+                 [slingshot "0.12.2"]
+                 [com.taoensso/timbre "4.10.0"]]
   :deploy-repositories [["clojars-https" {:url "https://clojars.org/repo"
                                           :username :env/clojars_user
                                           :password :env/clojars_password}]]
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[midje "1.9.0"]]
+             :dev {:dependencies [[midje "1.9.1"]]
                    :plugins [[lein-midje "3.2.1"]]}})
