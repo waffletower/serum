d0a68c0a13b3873a19cb7d86135c64f2ea492862
Christopher Penrose <waffletower@users.noreply.github.com>
2019-02-25 20:43:40 -0800

    up serum project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject waffletower/serum "0.6.0"
+(defproject waffletower/serum "0.7.0"
   :description "Clojure library of utility functions and macros"
   :url "https://github.com/waffletower/serum"
   :license {:name "Eclipse Public License"
