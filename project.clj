69ad3ebf22fdff2d3bbbb284cacebb32d19545a7
Christopher Penrose <waffletower@users.noreply.github.com>
2022-07-30 14:11:22 -0700

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject waffletower/serum "0.8.0"
+(defproject waffletower/serum "0.8.1"
   :description "Clojure library of utility functions and macros"
   :url "https://github.com/waffletower/serum"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "1.3.610"]
-                 [org.clojure/tools.logging "1.1.0"]
-                 [camel-snake-kebab "0.4.1"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.5.648"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [camel-snake-kebab "0.4.3"]]
   :deploy-repositories [["clojars-https" {:url "https://clojars.org/repo"
                                           :username :env/clojars_user
                                           :password :env/clojars_password}]]
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[midje "1.9.9"]]
+             :dev {:dependencies [[midje "1.10.5"]]
                    :plugins [[lein-midje "3.2.1"]]}})
