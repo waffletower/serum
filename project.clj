e02522e795d0a8fd1babe4a0bcd4ef573eb07af0
Christopher Penrose <waffletower@users.noreply.github.com>
2023-07-31 21:07:40 -0700

    Added 'map-exec' function. Updated project version to 0.10.0. Removed clojure.tools.logging usage.

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject waffletower/serum "0.9.2"
+(defproject waffletower/serum "0.10.0"
   :description "Clojure library of utility functions and macros"
   :url "https://github.com/waffletower/serum"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.5.648"]
-                 [org.clojure/tools.logging "1.2.4"]
                  [camel-snake-kebab "0.4.3"]]
   :deploy-repositories [["clojars-https" {:url "https://clojars.org/repo"
                                           :username :env/clojars_user
